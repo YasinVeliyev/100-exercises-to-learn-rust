526509435ac4f4cc8ba3f93ca8af2fb18ead89ee
YasinVeliyev <veliyev.yasin@gmail.com>
2025-01-14 00:04:46 +0400

    modified:   exercises/05_ticket_v2/15_outro/src/description.rs 	modified:   exercises/05_ticket_v2/15_outro/src/status.rs 	modified:   exercises/05_ticket_v2/15_outro/src/title.rs

--- exercises/05_ticket_v2/15_outro/src/status.rs
+++ exercises/05_ticket_v2/15_outro/src/status.rs
@@ -1,10 +1,37 @@
 // TODO: Implement `TryFrom<String>` and `TryFrom<&str>` for the `Status` enum.
 //  The parsing should be case-insensitive.
 
+#[derive(Debug, PartialEq, Clone)]
 pub enum Status {
     ToDo,
     InProgress,
     Done,
+}
+
+impl TryFrom<&str> for Status {
+    type Error = String;
+
+    fn try_from(value: &str) -> Result<Self, Self::Error> {
+        match &*value.to_lowercase() {
+            "todo" => Ok(Self::ToDo),
+            "inprogress" => Ok(Self::InProgress),
+            "done" => Ok(Self::Done),
+            _ => Err("Invalid String".to_owned()),
+        }
+    }
+}
+
+impl TryFrom<String> for Status {
+    type Error = String;
+
+    fn try_from(value: String) -> Result<Self, Self::Error> {
+        match &*value.to_lowercase() {
+            "todo" => Ok(Self::ToDo),
+            "inprogress" => Ok(Self::InProgress),
+            "done" => Ok(Self::Done),
+            _ => Err("Invalid String".to_owned()),
+        }
+    }
 }
 
 #[cfg(test)]
